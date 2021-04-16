e02cbf1dcf02ae483915ec30bceb7120023c1d38
Maciej Marczyszyn <248984@student.pwr.edu.pl>
2021-04-16 06:58:09 -0700

    kartkowka nr 4

---
+++ Kartkw/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Data.Entity;
+using System.Collections.Generic;
+
+
+
+
+
+
+namespace Kartkw
+{
+    class Student
+    {
+        public int ID { get; set; }
+        public int Deficyt { get; set; }
+
+        public string Name { get; set; }
+    }
+
+    class AiRW4 : DbContext
+    {
+        public virtual DbSet<Student> Students { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new AiRW4())
+            {
+                var s = new Student() { Deficyt = 11, Name="Chad" };
+
+                db.Students.Add(s);
+                db.SaveChanges();
+
+
+                foreach(var st in db.Students)
+                {
+                    Console.WriteLine($"ID: {st.ID} Imie: {st.Name} Deficy:{st.Deficyt}");
+
+                }
+                Console.ReadLine();
+
+
+            }
+        }
+    }
+}
